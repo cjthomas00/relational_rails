c611c951a41b5dcab37c5ee4c2517967df35af7d
Caleb Thomas <cjthomas00@comcast.net>
2023-01-31 17:20:33 -0700

    complete index test and show test for teams.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/players', to: 'players#index'
   get '/teams', to: 'teams#index'
-  # get '/teams/:id', to: 'teams#show'
+  get '/teams/:id', to: 'teams#show'
 end
