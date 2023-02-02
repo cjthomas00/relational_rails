2f2f925b5e584475679c8d2ba032fd30054ef551
Caleb Thomas <cjthomas00@comcast.net>
2023-02-02 14:00:44 -0700

    create self.order_teams test and method. Pass test.

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,8 @@
 class Team < ApplicationRecord
   has_many :players
+
+  def self.order_teams   # <=== use .self when calling a Class method
+    self.order(created_at: :desc)
+  end
 end
+
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Team, type: :model do
   it {should have_many :players}
+
+  describe 'instance methods' do
+    describe '##self.order_teams' do
+      before :each do
+        
+        @team_1 = Team.create!({name: 'Bruins', city: 'Boston', original_six_team: true, number_of_stanley_cups: 6})
+        sleep(1)
+        @team_2 = Team.create!({name: 'Avalanche', city: 'Colorado', original_six_team: false, number_of_stanley_cups: 3})
+        sleep(2)
+        @team_3 = Team.create!({name: 'Wild', city: 'Minnesota', original_six_team: false, number_of_stanley_cups: 0})
+        #@teams = [@team_1, @team_2, @team_3]
+      end
+      it 'can order teams by most recently created' do
+        expect(Team.order_teams).to eq([@team_3, @team_2, @team_1])
+      end
+    end
+  end
 end
