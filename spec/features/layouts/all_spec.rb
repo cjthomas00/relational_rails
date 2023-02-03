bc2f2fd3690291f60ebb3b976b88486cd89b74ca
Caleb Thomas <cjthomas00@comcast.net>
2023-02-03 15:25:22 -0700

    create test data for parent index link.

--- spec/features/layouts/all_spec.rb
+++ spec/features/layouts/all_spec.rb
@@ -18,6 +18,22 @@ RSpec.describe 'All Pages' do
         visit "/players/#{player1.id}"
         expect(page).to have_link('Players Index', href: '/players')
       end
+
+      it 'I see a link for the parent index' do
+        team_1 = Team.create!({name: 'Bruins', city: 'Boston', original_six_team: true, number_of_stanley_cups: 6})
+        player1 = Player.create!(name: "Bobby Orr", jersey_number: 4, retired: true, team: team_1)
+        
+        visit "/teams"
+        expect(page).to have_link('Teams Index', href: '/teams')
+        visit "/teams/#{team_1.id}"
+        expect(page).to have_link('Teams Index', href: '/teams')
+        visit "/teams/#{team_1.id}/players"
+        expect(page).to have_link('Teams Index', href: '/teams')
+        visit "/players"
+        expect(page).to have_link('Teams Index', href: '/teams')
+        visit "/players/#{player1.id}"
+        expect(page).to have_link('Teams Index', href: '/teams')
+      end
     end
   end
 end
