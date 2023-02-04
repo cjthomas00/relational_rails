0059e830f700b8d506ff4227377a7d466f9ce937
Caleb Thomas <cjthomas00@comcast.net>
2023-02-04 10:00:15 -0700

    add #new and #create.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -6,4 +6,13 @@ class TeamsController < ApplicationController
   def show
     @team = Team.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    team = Team.create(name: params[:name], city: params[:city], original_six_team: params[:Original_Six_Team], number_of_stanley_cups: params[:Stanley_Cups])
+
+    redirect_to "/teams/#{team.id}"
+  end
 end
