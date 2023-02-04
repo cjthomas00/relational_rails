02252f448de573deb2181af150f9eb2f15ced2ff
Caleb Thomas <cjthomas00@comcast.net>
2023-02-04 09:59:25 -0700

    Fill out test data for user story 11. Pass tests.

--- spec/features/teams/index_spec.rb
+++ spec/features/teams/index_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'the teams index page', type: :feature do
   describe "As a visitor" do
     before :each do 
       @team_1 = Team.create!({name: 'Bruins', city: 'Boston', original_six_team: true, number_of_stanley_cups: 6})
-      sleep(1)
+      #sleep(1)
       @team_2 = Team.create!({name: 'Avalanche', city: 'Colorado', original_six_team: false, number_of_stanley_cups: 3})
       # sleep(1)
       @team_3 = Team.create!({name: 'Wild', city: 'Minnesota', original_six_team: false, number_of_stanley_cups: 0})
@@ -40,23 +40,48 @@ RSpec.describe 'the teams index page', type: :feature do
 
         visit '/teams'
 
-        expect(page).to have_link('New Team', href:"/teams/#{@team_1.id}/players")
+        expect(page).to have_link('New Team', href:"/teams/new")
       end
 
       it 'When I click this link
       Then I am taken to "/parents/new" where I  see a form for a new parent record' do
 
+        visit '/teams'
+
+        click_link("New Team")
+        expect(current_path).to eq('/teams/new')
+        
       end
 
       it "When I fill out the form with a new parent's attributes:
       And I click the button 'Create Parent' to submit the form
       Then a `POST` request is sent to the '/parents' route" do
+        
 
+        visit '/teams/new'
+        fill_in('Name', with: 'Golden Knights') 
+        fill_in('City', with: 'Las Vegas')
+        fill_in('Original_Six_Team', with: false)
+        fill_in('Stanley_Cups', with: 0)
+        click_button('Create Team')
+
+        new_team_id = Team.last.id
+        expect(current_path).to eq("/teams/#{new_team_id}")
+        expect(page).to have_content("Golden Knights")
       end
 
       it "a new parent record is created,
       and I am redirected to the Parent Index page where I see the new Parent displayed." do
-        
+
+        visit '/teams/new'
+        fill_in('Name', with: 'Golden Knights') 
+        fill_in('City', with: 'Las Vegas')
+        fill_in('Original_Six_Team', with: false)
+        fill_in('Stanley_Cups', with: 0)
+        click_button('Create Team')
+
+        visit '/teams'
+        expect(page).to have_content("Golden Knights")
       end
     end
   end
