da6d3872d7989bbaa2f46a32ed0e854aecb47ca3
Caleb Thomas <cjthomas00@comcast.net>
2023-02-06 13:05:25 -0700

    create delete_spec file.

---
+++ spec/features/teams/delete_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'Teams Index Page' do
+  describe 'As a visitor' do
+    describe 'When I visit a parent show page' do
+      it 'Then I see a link to delete the parent' do
+        
+      end
+    end
+  end
+end
