0d4e4c418c21b53ad37a57ae895fad5cf14db77d
Caleb Thomas <cjthomas00@comcast.net>
2023-02-06 19:48:57 -0700

    change from delete to destroy_spec, add #destroy tests and pass.

--- spec/features/teams/destroy_spec.rb
+++ spec/features/teams/destroy_spec.rb
@@ -10,12 +10,28 @@ RSpec.describe 'Teams Index Page' do
       @player2 = Player.create!(name: "Joe Sakic", jersey_number: 19, retired: true, team: @team_2)
       @player3 = Player.create!(name: "Marion Gaborik", jersey_number: 10, retired: true, team: @team_3)
       @player4 = Player.create!(name: "Bobby Orr", jersey_number: 4, retired: true, team: @team_1)
+      @player5 = Player.create!(name: "Patrice Bergeron", jersey_number: 37, retired: false, team: @team_1)
     end
     describe 'When I visit a parent show page' do
       it 'Then I see a link to delete the parent' do
         visit "/teams/#{@team_1.id}"
-
+        
         expect(page).to have_link("Delete #{@team_1.name}")
+      end
+
+      it "When I click the link 'Delete Parent'
+      Then a 'DELETE' request is sent to '/parents/:id',
+      the parent is deleted, and all child records are deleted
+      and I am redirected to the parent index page where I no longer see this parent" do
+        visit "/teams/#{@team_1.id}"
+        click_link("Delete #{@team_1.name}")
+
+        expect(current_path).to eq("/teams")
+        expect(page).to_not have_content(@team_1.name)
+
+        visit "/players"
+        expect(page).to_not have_content(@player4.name)
+        expect(page).to_not have_content(@player5.name)
       end
     end
   end
